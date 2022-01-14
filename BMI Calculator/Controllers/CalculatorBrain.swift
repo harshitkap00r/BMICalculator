0c9fbe71d121b739bcc24b751eb71d1cb0723cdc
Harshit Kapoor <harshit.kapoor@getmyparking.com>
2022-01-14 14:22:28 +0530

    last

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Get My Parking on 14/01/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Get My Parking on 13/01/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+    
+    let bmiValue
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
